8c05559f5ca8e645741fe49493708301477a7fe1
unknown <dainnaxjakarta91@gmail.com>
2023-06-04 13:59:04 +0700

    dfa

--- routes.js
+++ routes.js
@@ -2,10 +2,6 @@ const express = require("express");
 
 const route = express.Router();
 
-route.get("/", (req, res) => {
-  res.send("ini /");
-});
-
 route.get("/api/news", (req, res) => {
   res.send("ini /api/news");
 });
